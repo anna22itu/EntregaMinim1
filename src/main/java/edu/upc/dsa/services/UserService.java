685db24fa940ed5d77e4d33ec9401d2322f9b797
anna22itu <anna.iturralde.aguilo@gmail.com>
2022-10-28 15:28:31 +0200

    avance

--- src/main/java/edu/upc/dsa/services/UserService.java
+++ src/main/java/edu/upc/dsa/services/UserService.java
@@ -14,6 +14,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
